d1ab87fd6b00310df65d69ee1867a7c0662b78a8
Hexaku <hex4ku@gmail.com>
2022-08-22 10:13:41 +0200

    edit potionIngredient fixtures to only add unique ingredients per recepe

--- src/DataFixtures/PotionIngredientFixtures.php
+++ src/DataFixtures/PotionIngredientFixtures.php
@@ -18,15 +18,22 @@ class PotionIngredientFixtures extends Fixture implements DependentFixtureInterf
         // For each potion, add 3 to 5 random ingredients with random quantities
         for($i=1; $i<=PotionFixtures::TOTAL_POTIONS; $i++){
             $randomPotionIngredients = rand(3,5);
-            for($j=0; $j<$randomPotionIngredients; $j++){
+            $uniqueIngredients = [];
+            for($j=1; $j<=$randomPotionIngredients; $j++){
                 $randomIngredientId = rand(0, count(IngredientFixtures::INGREDIENTS)-1);
                 $randomQuantity = rand(1, 5);
-                $newPotionIngredient = (new PotionIngredient())
-                    ->setPotion($this->getReference('potion_' . $i))
-                    ->setIngredient($this->getReference('ingredient_' . $this->slugifier->slugify(IngredientFixtures::INGREDIENTS[$randomIngredientId]['name'], '_')))
-                    ->setIngredientQuantity($randomQuantity);
+                $uniqueIngredients[] = $randomIngredientId;
+                $uniqueIngredients = array_unique($uniqueIngredients);
+                if(count($uniqueIngredients) === $j){
+                    $newPotionIngredient = (new PotionIngredient())
+                        ->setPotion($this->getReference('potion_' . $i))
+                        ->setIngredient($this->getReference('ingredient_' . $this->slugifier->slugify(IngredientFixtures::INGREDIENTS[$randomIngredientId]['name'], '_')))
+                        ->setIngredientQuantity($randomQuantity);
     
-                $manager->persist($newPotionIngredient);
+                    $manager->persist($newPotionIngredient);
+                } else {
+                    $randomPotionIngredients--;
+                }
             }
         }
 
