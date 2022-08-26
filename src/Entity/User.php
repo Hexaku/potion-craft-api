dc6d7e6ea78f639e8aced18c0571309f8d3bca6d
Hexaku <hex4ku@gmail.com>
2022-08-26 17:42:27 +0200

    add event subscriber for new user's password hashing

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,12 +7,14 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
+#[UniqueEntity('email', 'A user with this email adresse already exist')]
 #[ApiResource()]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
